37f184d845ab5a1cbb8a22785c994c09d2fbc656
Wren Morozova <siliconself@proton.me>
2024-04-06 19:54:02 -0600

    Reworked configuration to use a toml file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,9 @@
 //! Contains global program configuration
 
-use figment::{providers::Env, Figment};
+use figment::{
+    providers::{Format, Toml},
+    Figment,
+};
 use once_cell::sync::Lazy;
 use serde::Deserialize;
 
@@ -11,21 +14,20 @@ pub(crate) static APP_CONFIG: Lazy<Config> = Lazy::new(Config::new);
 #[derive(Deserialize)]
 pub(crate) struct Config {
     /// The path to the postgres database
-    database_path: String,
+    database_url: String,
 }
 
 impl Config {
     /// Create a new program configuration from the environment
     pub(crate) fn new() -> Self {
-        let config: Config = Figment::new()
-            .merge(Env::prefixed("WAITER_"))
+        let config: Config = Figment::from(Toml::file("waiter_config.toml"))
             .extract()
             .expect("Failed to configure program");
         config
     }
 
     /// Get a reference to the internal `database_path`
-    pub(crate) fn get_database_path(&self) -> &String {
-        &self.database_path
+    pub(crate) fn get_database_url(&self) -> &String {
+        &self.database_url
     }
 }
